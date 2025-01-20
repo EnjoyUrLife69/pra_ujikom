35623274362b7a618af64207791467aff120f553
Moh Bisma Targaryen <147211527+EnjoyUrLife69@users.noreply.github.com>
2025-01-20 13:52:45 +0700

    Einloggen und registrieren

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,32 @@
+import 'dart:async';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:praujikom/app/modules/dashboard/views/dashboard_view.dart';
+import 'package:praujikom/app/modules/login/views/login_view.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  late Timer _pindah; // Deklarasi Timer sebagai variabel kelas
+  final authToken = GetStorage();
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
+    // Membuat timer untuk memindahkan layar ke LoginView
+    _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => authToken.read('token') == null
+          ? Get.off(
+              () => const LoginView(),
+              transition: Transition.leftToRight,
+            )
+          : Get.off(() => const DashboardView()),
+    );
   }
 
   @override
   void onClose() {
+    // Hentikan timer saat controller tidak digunakan lagi
+    _pindah.cancel();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,18 @@
 import 'package:get/get.dart';
 
+import '../modules/register/bindings/register_binding.dart';
+import '../modules/register/views/register_view.dart';
+
+import '../modules/dashboard/bindings/dashboard_binding.dart';
+import '../modules/dashboard/views/dashboard_view.dart';
+
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+
+
 
 part 'app_routes.dart';
 
@@ -16,5 +27,21 @@ class AppPages {
       page: () => const HomeView(),
       binding: HomeBinding(),
     ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => RegisterView(),
+      binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.DASHBOARD,
+      page: () => const DashboardView(),
+      binding: DashboardBinding(),
+    ),
+    
   ];
 }
